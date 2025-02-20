eaf6d611e6096f66cdc3aa1bf17d72c81945e4c4
Caio Vinícius <caioviniciuscordeirodoamaral@gmail.com>
2025-02-19 23:23:49 -0300

    Genesis-Robotics-Hub

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-document.getElementById("scrollToTop").addEventListener("click",function(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("scrollToTop").addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     });
+    document.getElementById("acronym-cagr").addEventListener("mouseenter", function (){
+        message.style.display = 'block';
+    })
 });
